3f35db84d756ee381cc21d626b4c0111eaa854eb
Denisa <denisa.iordache@getik.net>
2024-10-03 11:49:13 +0300

    Solution

---
+++ src/app/success-alert/success-alert.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-success-alert',
+  templateUrl: './success-alert.component.html',
+  // i choose this to practice with css classes
+  styles: `
+  .yay {
+    color: green;
+  }
+  `
+})
+export class SuccessAlertComponent {
+
+}
---
+++ src/app/warning-alert/warning-alert.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-warning-alert',
+  // I choose this because it's just a single row of code
+  template: `<p>Warning alert works!</p>`,
+  styleUrl: './warning-alert.component.css'
+})
+export class WarningAlertComponent {
+
+}
